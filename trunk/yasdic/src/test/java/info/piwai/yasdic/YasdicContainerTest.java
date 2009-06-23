c747c86e53800531449dc0db06b91cea3d40a6c5
py.ricau <py.ricau@32f01cb6-5e3f-11de-8a4b-256bef446c6c>
2009-06-23 21:34:36 +0000

    Added test file (no test) improving pom (informations + mvn plugins)

---
+++ trunk/yasdic/src/test/java/info/piwai/yasdic/YasdicContainerTest.java
@@ -0,0 +1,153 @@
+/**
+ * 
+ */
+package info.piwai.yasdic;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author pricau
+ * 
+ */
+public class YasdicContainerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#YasdicContainer()}.
+	 */
+	@Test
+	@Ignore
+	public void testYasdicContainer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#YasdicContainer(info.piwai.yasdic.YasdicContainer)}
+	 * .
+	 */
+	@Test
+	@Ignore
+	public void testYasdicContainerYasdicContainer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#getBean(java.lang.String)}.
+	 */
+	@Test
+	@Ignore
+	public void testGetBean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#define(java.lang.String, info.piwai.yasdic.YasdicContainer.BeanDef)}
+	 * .
+	 */
+	@Test
+	@Ignore
+	public void testDefineStringBeanDefOfQ() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#define(java.lang.String, boolean, info.piwai.yasdic.YasdicContainer.BeanDef)}
+	 * .
+	 */
+	@Test
+	@Ignore
+	public void testDefineStringBooleanBeanDefOfQ() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#define(java.lang.String, java.lang.Object)}
+	 * .
+	 */
+	@Test
+	@Ignore
+	public void testDefineStringObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#undefineBean(java.lang.String)}.
+	 */
+	@Test
+	@Ignore
+	public void testUndefineBean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#unstoreSingleton(java.lang.String)}
+	 * .
+	 */
+	@Test
+	@Ignore
+	public void testUnstoreSingleton() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link info.piwai.yasdic.YasdicContainer#undefineAllBeans()}.
+	 */
+	@Test
+	@Ignore
+	public void testUndefineAllBeans() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link info.piwai.yasdic.YasdicContainer#reset()}.
+	 */
+	@Test
+	@Ignore
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+}
